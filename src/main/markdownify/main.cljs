ae4bee5beb3f871a5847e778f23de468d11539bf
regul <regul@mantis>
2020-03-09 19:12:59 +0000

    feat: markdown editor with HTML preview

--- src/main/markdownify/main.cljs
+++ src/main/markdownify/main.cljs
@@ -1,10 +1,36 @@
 (ns markdownify.main
-  (:require [reagent.core :as reagent]))
+  (:require [reagent.core :as reagent]
+            ["showdown" :as showdown]))
+
+(defonce markdown (reagent/atom ""))
+
+(defonce showdown-converter (showdown/Converter.))
+
+(defn md->html [md]
+  (.makeHtml showdown-converter md))
 
 (defn app []
   [:div
-   [:h1 "Hello world!"]])
-   
+   [:h1 "Markdownify"]
+   [:div
+    {:style {:display :flex}}
+
+    [:div
+     {:style {:flex "1"}}
+     [:h2 "Markdown"]
+     [:textarea
+      {:on-change #(reset! markdown (-> % .-target .-value))
+       :value @markdown
+       :style {:resize "none"
+               :height "500px"
+               :width "100%"}}]]
+
+    [:div
+     {:style {:flex "1"
+              :padding-left "2em"}}
+     [:h2 "HTML Preview"]
+     [:div {:style {:height "500px"}
+            :dangerouslySetInnerHTML {:__html (md->html @markdown)}}]]]])
 
 (defn mount! []
   (reagent/render [app]
