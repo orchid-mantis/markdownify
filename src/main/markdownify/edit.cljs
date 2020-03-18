5be56f2e1714fc5f973094dd8a3046f19b79a986
regul <regul@mantis>
2020-03-18 23:14:17 +0100

    feat: continuous editing
    Re-select edited text (via hotkeys), so you can keep editing.

--- src/main/markdownify/edit.cljs
+++ src/main/markdownify/edit.cljs
@@ -1,26 +1,36 @@
 (ns markdownify.edit)
 
+(defn selected-range [el]
+  {:start (.-selectionStart el)
+   :end (.-selectionEnd el)})
+
 (defn selected-text [el]
-  (let [start (.-selectionStart el)
-        end (.-selectionEnd el)]
+  (let [{:keys [start end]} (selected-range el)]
     (. (.-value el) (slice start end))))
 
 ; https://stackoverflow.com/questions/3964710/replacing-selected-text-in-the-textarea
 (defn replace-selected-text [el text]
-  (let [start (.-selectionStart el)
-        end (.-selectionEnd el)]
+  (let [{:keys [start end]} (selected-range el)]
     (set! (.-value el) (str (. (.-value el) (slice 0 start))
                             text
                             (. (.-value el) (slice end))))))
 
+(defn re-select [el text start]
+  (let [end (+ start (count text))]
+    (-> el .focus)
+    (-> el (.setSelectionRange start end))))
+
 (defn md-edit [text-state edit-fn]
   (let [el (.getElementById js/document "markdown-textarea")
+        {:keys [start]} (selected-range el)
         value (selected-text el)
         text (when (seq value) (. value trim))]
     (when (seq text)
-      (replace-selected-text el (edit-fn text))
-      (reset! text-state {:format :md
-                          :value (.-value el)}))))
+      (let [new-text (edit-fn text)]
+        (replace-selected-text el new-text)
+        (re-select el new-text start)
+        (reset! text-state {:format :md
+                            :value (.-value el)})))))
 
 (defn selected->italic [text-state]
   (md-edit text-state #(str "*" % "*")))
